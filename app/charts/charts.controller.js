2f51be74d2ca45dee2122cac599d9bb7e1c82e8b
Sean Cali <seankenjicali@gmail.com>
2017-03-15 16:51:09 -0700

    DRY up the code a bit

--- app/charts/charts.controller.js
+++ app/charts/charts.controller.js
@@ -1,90 +1,121 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('app')
-    .controller('ChartsController', ChartsController);
+    angular
+        .module('app')
+        .controller('ChartsController', ChartsController);
 
-  ChartsController.$inject = ['inqstatsFactory'];
+    ChartsController.$inject = ['inqstatsFactory'];
 
-  /* @ngInject */
-  function ChartsController(inqstatsFactory) {
-    // Preface: Try to accomplish just one of the charts first.
+    /* @ngInject */
+    function ChartsController(inqstatsFactory) {
 
-    /* -- STEP 3 - CALL ON YOUR FACTORY FUNCTION --
-    The factory containing the function you wrote is being injected into this
-    controller on lines 8 and 11. (Remember the John Papa style guide!)
+        var vm = this;
 
-    You now need to call on that factory function. Here is a (non working!) example
-    of how you call a factory function.
+        vm.charts = [{
+            name: 'Big Mac Index',
+            key: 'bigmac_index'
+        }, {
+            name: 'Birth Rate',
+            key: 'birth_rate'
+        }, {
+            name: 'CO2 Emissions',
+            key: 'co2_emissions'
+        }, {
+            name: 'Death Rate',
+            key: 'death_rate'
+        }, {
+            name: 'Corruption Index',
+            key: 'corruption_index'
+        }, {
+            name: 'Population Density',
+            key: 'density'
+        }, ];
 
-    ```js
-    factory
-      .get()
-      .then(function(data) {
-        // massage your data
-      })
-      .catch(function(error) {
-        console.log(error);
-      })
-    ```
-    */
 
-    /* -- STEP 4 --
-    Next, inside of your callback, you will need to massage your data.
+        vm.chart = {
+            data: {},
+            labels: {}
+        };
 
-    > What does he mean by massaging data? Sounds uncomfortable..
+        inqstatsFactory
+            .getData(vm.charts.map(function(e) {
+                return e.key;
+            }).join(','))
+            .then(function(data) {
 
-    It's not so bad. Here's the problem massaging solves.
+                vm.charts.forEach(function(charts) {
 
-    Angular Charts needs an array of objects that look like this so that it can
-    render your charts for you.
+                    vm.chart.data = data[charts.key].map(function(k) {
+                        return k.data;
+                    })
 
-    [
-      { x: 2014, y: 4.4 },
-      { x: 2015, y: 4.1 },
-      { x: 2016, y: 3.8 }
-    ]
+                    vm.chart.labels = data[charts.key].map(function(j) {
+                        return j.year;
+                    })
 
-    However, the data you'll receive from inqstatsFactory looks like this.
-
-    [
-      { year: 2014, data: 4.4 },
-      { year: 2015, data: 4.1 },
-      { year: 2016, data: 3.8 }
-    ]
-
-    See the difference?
-
-    We need to convert the data structure returned by inqstatsFactory
-    into the structure that Angular Charts requires.
-
-    In coding world we call this process "mapping". Converting one data structure
-    into another. As we are in JavaScript, mapping is achieved by using none other
-    than the `.map` function. (Don't forget to read the documentation in step 0!)
-
-    In the callback of your factory call, write the code to map the INQStats data
-    into Angular Chart data.
-    */
-
-    /* -- STEP 5: GIVE THE DATA TO ANGULAR CHART -->
-
-    Done that? Great!
-
-    Next up, for Angular Chart, you only need to use the `chart-data` and
-    `chart-label` directives in order to get one up and running.
-
-    You'll need a data structure like this to house the massaged data from step 2.
-
-    ```js
-    vm.bigMac = {
-      data: [4.4, 4.1, 3.8] // while literal in this example, this will really be the result of the call to the .map function.
-      labels: ['2014', '2015', '2016'] // see above comment, same applys here.
+                    // create new object to return? then pull from the data and labels from that ????
+                    // or do i need to ng-repeat chart for charts?
+                    
+                })
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
     }
-    ```
-
-    Once you've finished all this, you can move to ../index.html for Step 6 - building the UI!
-    */
-
-  }
 })();
+/* -- STEP 4 --
+Next, inside of your callback, you will need to massage your data.
+
+> What does he mean by massaging data? Sounds uncomfortable..
+
+It's not so bad. Here's the problem massaging solves.
+
+Angular Charts needs an array of objects that look like this so that it can
+render your charts for you.
+
+[
+  { x: 2014, y: 4.4 },
+  { x: 2015, y: 4.1 },
+  { x: 2016, y: 3.8 }
+]
+
+However, the data you'll receive from inqstatsFactory looks like this.
+
+[
+  { year: 2014, data: 4.4 },
+  { year: 2015, data: 4.1 },
+  { year: 2016, data: 3.8 }
+]
+
+See the difference?
+
+We need to convert the data structure returned by inqstatsFactory
+into the structure that Angular Charts requires.
+
+In coding world we call this process "mapping". Converting one data structure
+into another. As we are in JavaScript, mapping is achieved by using none other
+than the `.map` function. (Don't forget to read the documentation in step 0!)
+
+In the callback of your factory call, write the code to map the INQStats data
+into Angular Chart data.
+*/
+
+/* -- STEP 5: GIVE THE DATA TO ANGULAR CHART -->
+
+Done that? Great!
+
+Next up, for Angular Chart, you only need to use the `chart-data` and
+`chart-label` directives in order to get one up and running.
+
+You'll need a data structure like this to house the massaged data from step 2.
+
+```js
+vm.bigMac = {
+  data: [4.4, 4.1, 3.8] // while literal in this example, this will really be the result of the call to the .map function.
+  labels: ['2014', '2015', '2016'] // see above comment, same applys here.
+}
+```
+
+Once you've finished all this, you can move to ../index.html for Step 6 - building the UI!
+*/
--- gulpfile.js
+++ gulpfile.js
@@ -3,40 +3,41 @@ const inject = require('gulp-inject');
 const wiredep = require('wiredep').stream;
 const browserSync = require('browser-sync');
 
+/* injects the files that you create */
 gulp.task('inject', ['inject:bower'], function() {
-  var sources = gulp.src([
-    './app/**/*.module.js',
-    './app/**/*.js',
-    './app/**/*.css',
-    '!./app/bower_components/**/*.css',
-    '!./app/bower_components/**/*.js',
-  ]);
+    var sources = gulp.src([                                        // Define the files we'd like to inject
+        './app/**/*.module.js',                         // *.module.js files go first
+        './app/**/*.js',                                // *.js files come next
+        './app/**/*.css'                                // *.css files come next
+    ]);
 
-  return gulp
-    .src('./app/index.html')
-    .pipe(inject(sources, {relative: true}))
-    .pipe(gulp.dest('./app/'));
+    return gulp.src('./index.html')                             // Read your index.html
+        .pipe(inject(sources, { relative: true }))      // Pipe it through gulp-inject
+        .pipe(gulp.dest('./'));                         // Write it out to ./
 });
 
+/* injects your bower dependencies */
 gulp.task('inject:bower', function() {
-  return gulp
-    .src('./app/index.html')
-    .pipe(wiredep())
-    .pipe(gulp.dest('./app/'));
+    return gulp.src('./index.html')                     // Read your index.html
+        .pipe(wiredep())                                // Pipe it through wiredep
+        .pipe(gulp.dest('./'));                         // Write it out to ./
 });
 
+/* watches for changes made to your files */
 gulp.task('watch', function() {
-  return gulp.watch('./app/**/*', ['reload']);
+    return gulp
+        .watch('./app/**/*', ['reload']);               // Watch for any changes in your app folder
+                                                        // which will trigger the 'reload' task
 });
 
+/* reloads the browser */
 gulp.task('reload', function() {
-  return browserSync.reload();
+    return browserSync.reload();                        // reloads your app through browser-sync
 })
 
-gulp.task('serve', [
-  'inject', 'watch',
-], function() {
-  return browserSync.init({ // starts a browser-sync service
-    server: './app/' // serving the ./ directory
-  })
+/* starts a browser-sync server */
+gulp.task('serve', ['inject', 'watch'], function() {
+    return browserSync.init({                           // starts a browser-sync service
+        server: './'                                    // serving the ./ directory
+    })
 });
